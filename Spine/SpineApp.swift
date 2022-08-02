07f575c3567f788411e7fef7072f8e81cdb4c5a1
ailidhk11 <ailidhkinney@gmail.com>
2022-08-02 10:43:09 +0100

    Initial Commit

---
+++ Spine/SpineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpineApp.swift
+//  Spine
+//
+//  Created by Ailidh Kinney on 02/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
